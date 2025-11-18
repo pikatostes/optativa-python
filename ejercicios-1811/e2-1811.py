c00ee48d0722cc868032f1676c8ea22a553e911a
Alejandro Rios <alejandro.rios.cyt@gmail.com>
2025-11-18 16:26:59 +0100

    Ejercicios 18/11 (Corrección)

--- ejercicios-1811/e2-1811.py
+++ ejercicios-1811/e2-1811.py
@@ -1,9 +1,9 @@
 height = int(input("Introduce the height: "))
 
-for i in range(0, height):
+for i in range(height):
     if i == 0:
         print("4")
-    elif i == height-1:
+    elif i == height - 1:
         print("4 " * height)
     else:
-        print("4" + " " * (i+1) + "4")
+        print("4" + " " * (2*i - 1) + "4")
